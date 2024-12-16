834441b2b044a0dadc53c6452558a3a82585b732
Ueno <153161447+Uenoooooo@users.noreply.github.com>
2024-12-16 15:16:31 +0700

    Add files via upload

---
+++ Models/Reminder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reminder extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'reminder_name',
+        'reminder_desc',
+        'reminder_amount',
+        'start_date',
+        'created_at',
+        'updated_at',
+        'frequency_details'
+    ];
+
+ public function user(): BelongsTo{
+    return $this->belongsTo(User::class);
+ }
+
+ public function type(): BelongsTo{
+    return $this->belongsTo(Type::class);
+ }
+
+public function frequency(): BelongsTo{
+    return $this->belongsTo(Frequency::class);
+ }
+}
